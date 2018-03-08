36587a20c5c3de92be018153a5ab2b0f397aa493
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-03-07 22:52:24 -0700

    iterator to see if change took effect

--- assets/javascript/rpsm.js
+++ assets/javascript/rpsm.js
@@ -70,7 +70,7 @@ initApp = function () {
 			$("#firebaseui-auth-container").show();
 			$("body").removeClass("hotBody");
 			$("#mainArea").html('');
-			console.log("User is signed out 006");
+			console.log("User is signed out 007");
 		}
 	}, function (error) {
 		console.log(error);
