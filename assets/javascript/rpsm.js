78965bae1531e7eafaa74c046a1a796a50e261e6
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-03-07 22:28:13 -0700

    added in ui.js

--- assets/javascript/rpsm.js
+++ assets/javascript/rpsm.js
@@ -70,7 +70,7 @@ initApp = function () {
 			$("#firebaseui-auth-container").show();
 			$("body").removeClass("hotBody");
 			$("#mainArea").html('');
-			console.log("User is signed out 005");
+			console.log("User is signed out 006");
 		}
 	}, function (error) {
 		console.log(error);
---
+++ assets/javascript/ui.js
@@ -0,0 +1,56 @@
+"use strict";
+
+//Module for layouts and UI controls
+var UI = (function () {
+
+	// Exposed functions
+	return {
+		// Make Layout fill div vertically
+		fillVertically: function () {
+			//fill vertically
+			var pageHeader = document.querySelector("header");
+			var chat = document.querySelector("#chat");
+			chat.style.height = (window.innerHeight - pageHeader.clientHeight - 32) + "px";
+
+			//expand out chat area
+			var messages = document.querySelector("#chat-messages");
+			var chatHeader = document.querySelector("#chat-header");
+			var input = document.querySelector("#chat-input");
+
+			messages.style.height = (chat.clientHeight - (chatHeader.clientHeight + input.clientHeight)) + "px";
+
+			//manage image height -- Probably won't need this
+			// var images = document.querySelectorAll("#game .face");
+			// var height = (chat.clientHeight / 2) - 80;
+			// for (var counter = 0; counter < images.length; counter++) {
+			// 	var img = images.item(counter);
+			// 	img.style.maxHeight = height + "px";
+			// }
+		},
+
+		/*
+		 * Simple way to show an MDL snackbar at the bottom of the page
+		 * https://getmdl.io/components/index.html#snackbar-section
+		 * */
+		snackbar: function (data) {
+			document.querySelector("#snackbar").MaterialSnackbar.showSnackbar(data);
+		},
+
+		/*
+		 * Initialization for some generic ui elements
+		 * */
+		init: function () {
+			// help dialog -- Probably won't need this
+			// document.querySelector("#help").addEventListener("click", function () {
+			// 	document.querySelector('#help-dialog').showModal();
+			// });
+
+			// play again dialog
+			var result = document.querySelector("#result");
+			result.querySelector("button").addEventListener("click", function () {
+				//result.close();
+				window.location.reload();
+			});
+		}
+	}
+})();
