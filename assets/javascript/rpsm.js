5f6b71751b3cde3e0205381343f3a70e2244bc68
Kelly Costner <32380985+belobig@users.noreply.github.com>
2018-03-08 17:05:58 -0700

    added header tag to navbar in html

--- assets/javascript/rpsm.js
+++ assets/javascript/rpsm.js
@@ -70,7 +70,7 @@ initApp = function () {
 			$("#firebaseui-auth-container").show();
 			$("body").removeClass("hotBody");
 			$("#mainArea").html('');
-			console.log("User is signed out 015");
+			console.log("User is signed out 016");
 		}
 	}, function (error) {
 		console.log(error);
